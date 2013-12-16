374d6fd324e2abb3621a5dd2ca81af7628da7a1b
Nick Davies <git@nicolasdavies.com.au>
2013-12-16 14:44:06 +1100

    Example of how to do what you wanted

--- myfirst.go
+++ myfirst.go
@@ -1,18 +1,28 @@
 package main
 
 /*
-#include <svm.h>
+#include "svm.h"
 */
-
 import "C"
+import "unsafe"
 
 func main() {
-    a := C.struct_parts{i:C.int(1), j:C.int(2)}
-    b := C.struct_parts{i:C.int(2), j:C.int(3)}
-    c := C.struct_parts{i:C.int(3), j:C.int(4)}
-    d := C.struct_parts{i:C.int(4), j:C.int(5)}
-    e := C.struct_parts{i:C.int(5), j:C.int(6)}
-    f := C.struct_parts{i:C.int(6), j:C.int(7)}
 
-    //How do I then create checking_pointers?
+    godata := [][]C.struct_parts{
+        []C.struct_parts{
+            C.struct_parts{i: 1, j: 2},
+            C.struct_parts{i: 2, j: 3},
+            C.struct_parts{i: 3, j: 4},
+        },
+        []C.struct_parts{
+            C.struct_parts{i: 4, j: 5},
+            C.struct_parts{i: 5, j: 6},
+            C.struct_parts{i: 6, j: 7},
+        },
+    }
+
+    var _ = C.struct_checking_pointers{
+        x: (**C.struct_parts)(unsafe.Pointer(&godata[0])),
+    }
+
 }
